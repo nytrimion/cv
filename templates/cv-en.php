50fe4ce2f552a56291a7cbc70dbb95671cbff4df
David Oury <oury.david@gmail.com>
2025-09-24 13:49:54 +0200

    refactor: adjust profile section

--- templates/cv-en.php
+++ templates/cv-en.php
@@ -9,12 +9,7 @@
 <body>
 <div class="cv-container">
     <div class="sidebar">
-        <div class="profile-section">
-            <div class="profile-image"></div>
-            <h1 class="name">David OURY</h1>
-            <div class="title"><?= $title ?></div>
-            <div class="subtitle">Modern Stack Advocate</div>
-        </div>
+        <?php require 'profile-section.php' ?>
 
         <div class="about-section">
             <div>Tech is only a means, not an end in itself.</div>
---
+++ templates/profile-section.php
@@ -0,0 +1,11 @@
+<div class="profile-section">
+    <div class="profile-identity">
+        <div class="profile-image"></div>
+        <div class="profile-name">
+            <div class="firstname">David</div>
+            <div class="lastname">OURY</div>
+        </div>
+    </div>
+    <div class="title"><?= $title ?></div>
+    <div class="subtitle">Modern Stack Advocate</div>
+</div>
